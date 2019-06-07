b76855ecb45e3185a9184d459e37963279d764f5
Colton James <coltonlj@uw.edu>
2019-06-07 14:27:34 -0700

    Updeated scripts again/added photos folder

--- Scripts/Attach.cs
+++ Scripts/Attach.cs
@@ -18,7 +18,7 @@ public class Attach : MonoBehaviour
     Valve.VR.InteractionSystem.Hand currentHand;// = new Valve.VR.InteractionSystem.Hand();
     AudioSource attachSound;
 
-    bool hasJoint = false;
+    //bool hasJoint = false;
 
     // Start is called before the first frame update
     void Start()
@@ -36,34 +36,35 @@ public class Attach : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Rigidbody>() != null && //Checking for the other objects rigidbody
-            collision.gameObject.tag == "NodeType" && hasJoint == false) //&& Checking for Node tag
+            collision.gameObject.tag == "NodeType")// && hasJoint == false) //&& Checking for Node tag
         {
             FixedJoint fj = gameObject.AddComponent<FixedJoint>(); //initialize FixedJoint
             fj.connectedBody = collision.gameObject.GetComponent<Rigidbody>(); //Attach FixedJoint to other rigidbody
 
+            TextMeshPro otherText = collision.transform.Find("dataText").GetComponent<TextMeshPro>();
+            TMPInput.text = otherText.text; //Change the text displayed on the TMPInput object
+
             currentHand.TriggerHapticPulse(10000);
             attachSound.Play();
-            hasJoint = true;
+            //hasJoint = true;
         }
     }
 
     /*
      * OnCollisionStay finds the correct input from the node being collided with, and updates the text on its parents node.
      */
-    private void OnCollisionStay(Collision collision)
-    {
-        TextMeshPro otherText = collision.transform.Find("dataText").GetComponent<TextMeshPro>();
-        TMPInput.text = otherText.text; //Change the text displayed on the TMPInput object
-    }
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //}
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if(TMPInput.text != null)
-        {
-            TMPInput.text = "NULL";
-            hasJoint = false;
-            Debug.Log("OnCollisionExit was called");
-        }
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if(TMPInput.text != null)
+    //    {
+    //        TMPInput.text = "NULL";
+    //        hasJoint = false;
+    //        Debug.Log("OnCollisionExit was called");
+    //    }
 
-    }
+    //}
 }
